f87b0af316f60b6873eb9721f4587dea200e2002
Renan Santos <renansantosmendes@gmail.com>
2022-10-19 23:05:21 -0300

    updated test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 from fastapi.testclient import TestClient
-from main import app
+from main import app, model, scaler
 
 client = TestClient(app)
 
@@ -8,10 +10,18 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World new version"}
+    assert response.json() == {"Hello": "World"}
 
 
-def test_api_route():
-    response = client.get("/api")
-    assert response.status_code == 200
-    assert response.json() == {"key": "value"}
+# def test_api_route():
+#     response = client.get("/api/predict")
+#     assert response.status_code == 200
+#     assert response.json() == {"key": "value"}
+
+
+def test_model_instance():
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_scaler_instance():
+    assert isinstance(scaler, StandardScaler)
